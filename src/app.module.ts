19735a162cb4c3feba232d27428a8bd00eb83245
Yorkinjon10 <abdukhalilovyorkinjon2025@gmail.com>
2024-06-17 23:33:47 +0500

    $$it

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,8 @@ import { UserSchema } from './schemas/users.schema';
 import { UserService } from './users/users.service';
 // mongodb+srv://username:password@cluster0.mongodb.net/myDatabase?retryWrites=true&w=majority
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://abdukhalilovyorkinjon2025:9Sw-6jfE$aGh$G4@cluster0.edrvw4g.mongodb.net/', {
+  imports: [ 
+    MongooseModule.forRoot('mongodb://abdukhalilovyorkinjon2025:9Sw-6jfE$aGh$G4@cluster0.edrvw4g.mongodb.net/', { 
       dbName: 'nestjs_leetcode'
     }),
     MongooseModule.forFeature([{ name: 'Users', schema: UserSchema }]),
