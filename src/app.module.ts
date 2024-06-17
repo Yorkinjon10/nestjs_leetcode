64234632b9fa2ccb16494b263db0537a1b4bc3c0
Yorkinjon10 <abdukhalilovyorkinjon2025@gmail.com>
2024-06-17 19:58:50 +0500

    mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,16 @@ import { AppService } from './app.service';
 import { UserController } from './users/users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/users.schema';
-
+import { UserService } from './users/users.service';
+// mongodb+srv://username:password@cluster0.mongodb.net/myDatabase?retryWrites=true&w=majority
 @Module({
   imports: [
-    MongooseModule.forRoot( 'mongodb+srv://abdukhalilovyorkinjon2025:9Sw-6jfE$aGh$G4@cluster0.edrvw4g.mongodb.net/'),
+    MongooseModule.forRoot('mongodb://abdukhalilovyorkinjon2025:9Sw-6jfE$aGh$G4@cluster0.edrvw4g.mongodb.net/', {
+      dbName: 'nestjs_leetcode'
+    }),
     MongooseModule.forFeature([{ name: 'Users', schema: UserSchema }]),
   ],
-  controllers: [AppController, UserController],
-  providers: [AppService],
-})
+  controllers: [AppController, UserController], 
+  providers: [AppService, UserService],
+}) 
 export class AppModule {}
